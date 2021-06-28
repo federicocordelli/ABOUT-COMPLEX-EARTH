3d43b8a14f31e83617c4958addc39f667ef6d804
federicocordelli.github.io <cordelli.federico@gmail.com>
2021-06-28 03:59:04 +0200

    nn

--- sketch.js
+++ sketch.js
@@ -82,7 +82,7 @@ fill(colorText1);
 textAlign(LEFT);
 textSize(paragraphTextSize);
 textFont(intlMed);
-text("The storyline you are going to dive in \ninvolves two timesets, that we called, \nfor our own convenience,\nT1 (dystopian present on Earth) and T2 (Mycenae).\nIt’s 2033, and Earth is about to collapse \ndue to an invasive species of ants farmed \nby some visionary investors in order \nto initially contrast climate emergencies derived \nfrom intensive farming (cattle, poultry, etc.).", width/12,300);
+text("The storyline you are going to dive in \ninvolves two timesets, that we called, \nfor our own convenience,\nT1 (dystopian present on Earth) and T2 (Mycenae).\nIt’s 2033, and Earth is about to collapse \ndue to an invasive species of ants farmed \nby some visionary investors in order \nto initially contrast climate emergencies derived \nfrom intensive farming (cattle, poultry, etc.).", width/12,200);
 pop();
 
 push();
@@ -90,7 +90,7 @@ fill(colorText1);
 textAlign(LEFT);
 textSize(paragraphTextSize);
 textFont(intlMed);
-text("Susy Duval is an ex-astrophisic who has worked \nfor the Aeneas project. She was left on Earth, \nand now she's a lone survivor. \n\nTitus Van Houten lost his daughter and his wife\nduring the famine,\nand now he's trying to restart\na new life with his community.\n\nWhat will happen when these two characters will meet?", ((width/12)*7),300);
+text("Susy Duval is an ex-astrophisic who has worked \nfor the Aeneas project. She was left on Earth, \nand now she's a lone survivor. \n\nTitus Van Houten lost his daughter and his wife\nduring the famine,\nand now he's trying to restart\na new life with his community.\n\nWhat will happen when these two characters will meet?", ((width/12)*7),200);
 pop();
 
 imageMode(CORNERS);
@@ -111,7 +111,7 @@ fill(colorText1);
 textAlign(LEFT);
 textSize(paragraphTextSize);
 textFont(intlMed);
-text("T1 shows the experience of mankind on a dying Earth, \nwhere entomophagy was thought \nto be the salvation for humanity.\nIn particular, we follow the adventures \nof two main characters, \nSusy Duval and Titan van Houten, \nwhose lives intertwined because of the secret \nspace mission (known as Aeneas) that was meant \nto save a part of humanity in case \nof any upcoming Apocalypse.", ((width/12)*7)+(scrall*1.2),300);
+text("T1 shows the experience of mankind on a dying Earth, \nwhere entomophagy was thought \nto be the salvation for humanity.\nIn particular, we follow the adventures \nof two main characters, \nSusy Duval and Titan van Houten, \nwhose lives intertwined because of the secret \nspace mission (known as Aeneas) that was meant \nto save a part of humanity in case \nof any upcoming Apocalypse.", ((width/12)*7)+(scrall*1.2),200);
 pop();
 
 
